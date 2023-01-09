84a5aa02ef52e78d1b66779ca3411be3ffd71c6a
박서연 <seoyeonpub@gmail.com>
2023-01-09 11:19:51 +0900

    투두

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wanted-pre-onboarding-challenge-fe-1
+
+원티드 프론트앤드 온보딩 챌린지
---
+++ todo/src/auth/Login.tsx
@@ -0,0 +1,10 @@
+function Login() {
+    return <div>
+        <h1>로그인하세요</h1>
+        <input type="text" placeholder="이메일" />
+        <input type="password" placeholder="비밀번호" />
+        <button>제출</button>
+    </div>;
+}
+
+export default Login;
